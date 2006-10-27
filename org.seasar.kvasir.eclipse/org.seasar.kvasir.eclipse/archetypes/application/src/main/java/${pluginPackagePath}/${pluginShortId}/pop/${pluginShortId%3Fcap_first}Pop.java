765c1bfb95ead8eae5f6fb8ffc6e62cc02e4db8b
YOKOTA, Takehiko <skirnir@gmail.com>
2006-10-27 03:29:41 +0000

    archetype中のGenericPopのパッケージ名が最新のPopプラグインのものに追従できていなかったので修正した。
    git-svn-id: https://www.seasar.org/svn/sandbox/kvasir/eclipse/trunk@890 ccb6d941-20fb-0310-8948-d27bf48cceb9

--- org.seasar.kvasir.eclipse/org.seasar.kvasir.eclipse/archetypes/application/src/main/java/${pluginPackagePath}/${pluginShortId}/pop/${pluginShortId%3Fcap_first}Pop.java
+++ org.seasar.kvasir.eclipse/org.seasar.kvasir.eclipse/archetypes/application/src/main/java/${pluginPackagePath}/${pluginShortId}/pop/${pluginShortId%3Fcap_first}Pop.java
@@ -1,6 +1,6 @@
 package ${pluginId}.${pluginShortId}.pop;
 
-import org.seasar.kvasir.cms.pop.pop.impl.GenericPop;
+import org.seasar.kvasir.cms.pop.pop.GenericPop;
 
 
 public class ${pluginShortId?cap_first}Pop extends GenericPop
