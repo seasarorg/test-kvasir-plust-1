ccc5ad5a30adaa2287fb8233f75ceef190440c0b
YOKOTA, Takehiko <skirnir@gmail.com>
2007-01-15 17:02:46 +0000

    プロジェクトテンプレートが間違っていたので修正した。
    git-svn-id: https://www.seasar.org/svn/sandbox/kvasir/eclipse/trunk@1516 ccb6d941-20fb-0310-8948-d27bf48cceb9

--- plust/org.seasar.kvasir.plust/archetypes/application/src/main/java/${pluginPackagePath}/${pluginShortId}/pop/${pluginShortId%3Fcap_first}Pop.java
+++ plust/org.seasar.kvasir.plust/archetypes/application/src/main/java/${pluginPackagePath}/${pluginShortId}/pop/${pluginShortId%3Fcap_first}Pop.java
@@ -2,7 +2,10 @@ package ${pluginId}.${pluginShortId}.pop;
 
 import org.seasar.kvasir.cms.pop.pop.GenericPop;
 
+import ${pluginId}.${pluginClassName};
+
 
 public class ${pluginShortId?cap_first}Pop extends GenericPop
 {
+    public static final String ID = ${pluginClassName}.ID + ".${pluginShortId}Pop";
 }
