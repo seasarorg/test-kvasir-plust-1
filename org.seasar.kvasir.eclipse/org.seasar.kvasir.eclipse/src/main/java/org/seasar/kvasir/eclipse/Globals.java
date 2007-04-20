5a0bcee83d475f4253b72db9d4a715a10f1db8b5
YOKOTA, Takehiko <skirnir@gmail.com>
2007-04-20 14:31:44 +0000

    既に不要なファイルであるため削除した。
    git-svn-id: https://www.seasar.org/svn/sandbox/kvasir/eclipse/trunk@1918 ccb6d941-20fb-0310-8948-d27bf48cceb9

--- org.seasar.kvasir.eclipse/org.seasar.kvasir.eclipse/src/main/java/org/seasar/kvasir/eclipse/Globals.java
+++
@@ -1,8 +0,0 @@
-package org.seasar.kvasir.eclipse;
-
-public interface Globals
-{
-    String BUNDLENAME_TOMCATPLUGIN = "com.sysdeo.eclipse.tomcat";
-
-    String NATURE_ID_TOMCAT = BUNDLENAME_TOMCATPLUGIN + ".tomcatnature";
-}
