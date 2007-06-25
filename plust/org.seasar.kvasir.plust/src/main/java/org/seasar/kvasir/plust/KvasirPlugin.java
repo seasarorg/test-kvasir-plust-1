cf9759404cda68258f77d1a99fc42bc8b6d33e70
YOKOTA, Takehiko <skirnir@gmail.com>
2007-06-25 01:02:14 +0000

    Eclipse3.2.2で何かする度にExceptionがスローされてうまく動かない問題に対処した。
    git-svn-id: https://www.seasar.org/svn/sandbox/kvasir/eclipse/trunk@1949 ccb6d941-20fb-0310-8948-d27bf48cceb9

--- plust/org.seasar.kvasir.plust/src/main/java/org/seasar/kvasir/plust/KvasirPlugin.java
+++ plust/org.seasar.kvasir.plust/src/main/java/org/seasar/kvasir/plust/KvasirPlugin.java
@@ -158,6 +158,14 @@ public class KvasirPlugin extends AbstractUIPlugin
     public KvasirPlugin()
     {
         plugin = this;
+
+        imageRegistry_ = getImageRegistry();
+        imageRegistry_.put(IMG_REQUIRED, getImageDescriptor(IMG_REQUIRED));
+        imageRegistry_.put(IMG_LIBRARY, getImageDescriptor(IMG_LIBRARY));
+        imageRegistry_.put(IMG_EXTENSION_POINT,
+            getImageDescriptor(IMG_EXTENSION_POINT));
+        imageRegistry_.put(IMG_EXTENSION, getImageDescriptor(IMG_EXTENSION));
+        imageRegistry_.put(IMG_ELEMENT, getImageDescriptor(IMG_ELEMENT));
     }
 
 
@@ -168,14 +176,6 @@ public class KvasirPlugin extends AbstractUIPlugin
         throws Exception
     {
         super.start(context);
-
-        imageRegistry_ = new ImageRegistry();
-        imageRegistry_.put(IMG_REQUIRED, getImageDescriptor(IMG_REQUIRED));
-        imageRegistry_.put(IMG_LIBRARY, getImageDescriptor(IMG_LIBRARY));
-        imageRegistry_.put(IMG_EXTENSION_POINT,
-            getImageDescriptor(IMG_EXTENSION_POINT));
-        imageRegistry_.put(IMG_EXTENSION, getImageDescriptor(IMG_EXTENSION));
-        imageRegistry_.put(IMG_ELEMENT, getImageDescriptor(IMG_ELEMENT));
 
         try {
             console_ = new KvasirConsole();
