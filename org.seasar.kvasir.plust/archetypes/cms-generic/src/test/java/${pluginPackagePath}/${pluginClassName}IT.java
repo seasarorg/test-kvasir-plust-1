ede40399babcf872cd4431781ed56704ccc2a3f3
YOKOTA, Takehiko <skirnir@gmail.com>
2010-04-28 14:06:48 +0000

    Javadocコメントを追記した。
    git-svn-id: https://www.seasar.org/svn/sandbox/kvasir/plust/trunk@2231 ccb6d941-20fb-0310-8948-d27bf48cceb9

--- org.seasar.kvasir.plust/archetypes/cms-generic/src/test/java/${pluginPackagePath}/${pluginClassName}IT.java
+++ org.seasar.kvasir.plust/archetypes/cms-generic/src/test/java/${pluginPackagePath}/${pluginClassName}IT.java
@@ -15,6 +15,9 @@ public class ${pluginClassName}IT extends KvasirPluginTestCase<${pluginClassName
     public static Test suite()
         throws Exception
     {
-        return createTestSuite(${pluginClassName}IT.class);
+        // kvasir-webappに直接的にも間接的にも依存していないプラグインの場合は
+        // kvasir-webapp関連のクラスがないというエラーが発生することがあります。
+        // その場合は第二引数をfalseにして下さい。
+        return createTestSuite(${pluginClassName}IT.class, true);
     }
 }
